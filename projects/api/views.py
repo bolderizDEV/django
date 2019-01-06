683c81c944115f6ef56a74229b3c076f27885221
= <=>
2019-01-06 18:37:32 +0100

    last commit

--- projects/api/views.py
+++ projects/api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Employee
+from .serializers import EmployeeSerializer
 
-# Create your views here.
+
+class EmployeeList(APIView):
+
+    def get(self, request):
+        employee = Employee.objects.all()
+        serializer = EmployeeSerializer(employee, many=True)
+        return Response(serializer.data)
+
