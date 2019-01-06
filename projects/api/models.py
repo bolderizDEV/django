1da17b70eafc65796525c07d514828eb8f23c956
= <=>
2019-01-06 10:17:22 +0100

    New model Employee

--- projects/api/models.py
+++ projects/api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    employee_id = models.IntegerField(null=False)
+    first_name = models.CharField(max_length=20),
+    last_name = models.CharField(max_length=25, null=False),
+    email = models.CharField(max_length=25, null=False),
+    phone_number = models.CharField(max_length=20),
+    hire_date = models.DateField(null=False),
+    job_id = models.CharField(max_length=10, null=False),
+    salary = models.FloatField(max_length=8.2),
+    commission_pct = models.FloatField(max_length=2.2),
+    manager_id = models.IntegerField(),
+    department_id = models.IntegerField()
+
+
